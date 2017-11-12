f4daabcded756ebf084dca939c2fb7666f0125f3
Toby <tobyreed0@gmail.com>
2017-11-12 16:14:03 +0000

    Initial Commit incl. Nearest Neighbour Interpolation

---
+++ Task1.m
@@ -0,0 +1,40 @@
+% Setup for use
+clear; 
+close all; 
+clc;
+
+% % Step-1: Load input image
+I = imread('Zebra.jpg');
+
+% % Step-2: Conversion of input image to grey-scale image
+grayImage = rgb2gray(I);
+
+% Define a  new image and populate it with zeroes
+newImageCols = 1836;
+newImageRows = 1668;
+
+oldImageRows = 556;
+oldImageColumns = 612;
+
+% Create a new array of integers at the correct size
+imageNew = uint8(zeros([newImageRows, newImageCols]));
+
+% Figure out the scale of the image
+rowScale = round(size(imageNew, 1) / oldImageRows);
+colScale = round(size(imageNew, 2) / oldImageColumns);
+
+for rows = 0:newImageRows-2
+  for columns = 0:newImageCols-2
+      
+      newPixelR = 1+round(rows / rowScale);
+      newPixelC = 1+round(columns / colScale);
+      
+      nextRow = rows + 1;
+      nextCols = columns + 1;
+      
+      % Set the next value in the image
+      imageNew(nextRow, nextCols) = grayImage(newPixelR, newPixelC);
+  end
+end
+
+imshow(imageNew);
---
+++ Task2.m
@@ -0,0 +1,10 @@
+% MATLAB script for Assessment Item-1
+
+% Task-2
+clear; 
+close all;
+clc;
+
+% Step One - Read in the Image
+image = imread('SC.png');
+imshow(image)
