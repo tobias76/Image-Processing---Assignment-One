499013ef216058ba9b68cf691a5b5b8ab9577cbd
Toby <tobyreed0@gmail.com>
2017-11-15 12:27:45 +0000

    Added Sort
    Sort is slow.
    Need to filter.

--- Task3.m
+++ Task3.m
@@ -12,7 +12,7 @@ image = rgb2gray(imread('Noisy.png'));
 paddedCols = size(image, 1) + 4;
 paddedRows = size(image, 2) + 4;
 
-medSortArray = zeros(1,25);
+medSortArray = [1,25];
 
 sortCounter = 1;
 
@@ -27,12 +27,24 @@ for row = 3:size(image,1)-2
     for col = 3:size(image,2)-2       
         % Kernel Mask n Ting
         sum = 0;
-        sortCounter = 1;
-        sortCounter = sortCounter + 1;
+        sortCounter = 0;
         for rows = row-2:row+2
             for cols = col-2:col+2
+                sortCounter = sortCounter + 1;
                 sum = sum + (1/25 * x(rows, cols));
-                medSortArray(1, sortCounter) = x(rows, cols); 
+                
+                if sortCounter < 26
+                    medSortArray(1, sortCounter) = x(rows + 2, cols + 2);
+                end
+                
+                % Two Two Little Quick Sorts
+                pivotPoint = medSortArray(end);
+                medSortArray(end) = [];
+                
+                lessArray = medSortArray(medSortArray <= pivotPoint);
+                greaterArray = medSortArray(medSortArray > pivotPoint);
+                
+                medSortArray = [lessArray pivotPoint greaterArray];
             end
         end
         smoothedImage(row-2, col-2) = sum;
