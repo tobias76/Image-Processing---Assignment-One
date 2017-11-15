b3e695dba22d0d7134ee0193bfa5772c71ca28ec
Toby <tobias76@users.noreply.github.com>
2017-11-15 15:54:57 +0000

    Finished Task 3 and Started Task Four

--- Task4.m
+++ Task4.m
@@ -6,28 +6,17 @@ close all;
 clc;
 
 % Task One - Load in Image
-image = im2double(rgb2gray(imread('Starfish.jpg')));
+image = imread('Starfish.jpg');
+greyscaleImage = im2double(rgb2gray(image));
 
-image = medfilt2(image);
+medFiltImage = medfilt2(greyscaleImage, [5 5]);
 
-imshow
-imageRows = size(image, 1);
-imageCols = size(image, 2);
 
-for X = 1:imageRows - 2
-    for Y = 1:imageCols - 2
-         % Find the sobel for the X-Axis
-        xMask = ((2 * image(X + 2, Y + 1) + image(X + 2 , Y) + image(X+2, Y+2))-(2 * image(X, Y + 1) + image(X, Y) + image(X,Y+2)));
-        
-        % Find the sobel for the Y-Axis
-        yMask = ((2 * image(X + 1 ,Y + 2) + image(X,Y + 2)+ image(X + 2,Y + 2))- (2*image(X + 1,Y)+image(X,Y) + image(X + 2,Y)));
-    
-        imageOut(X, Y) = abs(xMask) + abs(yMask);
-   
-        xMaskApply(X, Y) = abs(xMask);
-        yMaskApply(X, Y) = abs(yMask);
-    end
-end
+figure;
+subplot(1,2,1);
+imshow(image);
+title('Unedited Image')
 
-imshow(imageOut);
-
+subplot(1,2,2);
+imshow(greyscaleImage);
+title('Greyscale Image')
